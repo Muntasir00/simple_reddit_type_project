ad1d6b9c28cfbc4958d74470a2890571b9980091
Muntasir00 <muntasir690000@gmail.c.com>
2021-12-03 20:33:28 +0600

    serializers and post api created

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from posts import serializers
+from rest_framework import generics
+from .serializers import PostSerializer
+from .models import Post
 # Create your views here.
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer 
--- simple_reddit_clone/settings.py
+++ simple_reddit_clone/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #apps
+    'rest_framework',
+    
+    #local apps
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
